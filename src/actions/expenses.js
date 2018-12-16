4e356b0dcc5e5277c26c70e137d7e88eb16bad53
Andrii Korol <aekorol777@gmail.com>
2018-12-16 18:02:43 +0200

    remove expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,6 +1,7 @@
 import uuid from 'uuid';
 import database from '../firebase/firebase';
 
+// ADD
 export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
   expense
@@ -25,18 +26,28 @@ export const startAddExpense = (expenseData = {}) => {
   };
 };
 
+// REMOVE
 export const removeExpense = ({id} = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
 });
 
+export const startRemoveExpense = ({id} = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then((snapshot) => {
+      dispatch(removeExpense({id}));
+    });
+  };
+};
+
+// EDIT
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
   updates,
   id
 });
 
-// SET_EXPENSES
+// SET
 export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
   expenses
